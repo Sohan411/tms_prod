9cd1f884e8d91432cd04aa141d2c0c81008a6662
Sohan Sarurkar <sohansarurkar47@gmail.com>
2023-12-07 20:32:39 +0530

    prod tms

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const router = require('./routes');
 const fs = require('fs');
 const bodyParser = require('body-parser');
 const SA = require('./superadmin/SA');
+const https = require('https')
 //const TMS_logs = require('./tms_trigger_logs');
 //const checkState = require('./SMS/smsController');
 
@@ -17,6 +18,12 @@ const SA = require('./superadmin/SA');
 /*                 MQTT                  */
 // const mqtt_sub = require('./sub');
 // const mqtt_pub = require('./pub');
+
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/tms.senselive.in/privkey.pem', 'utf8');
+const fullchain = fs.readFileSync('/etc/letsencrypt/live/tms.senselive.in/fullchain.pem', 'utf8');
+
+const credentials = { key: privateKey, cert: fullchain };
+
 const app = express();
 
 const port = 3000;
@@ -30,8 +37,13 @@ app.use(SA.log);
 // Use the router for handling routes
 app.use(router);
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+const httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(3000, () => {
+  console.log('HTTPS server listening on port 443');
 });
+// // Start the server
+// app.listen(port, () => {
+//   console.log(`Server running on port ${port}`);
+// });
 
