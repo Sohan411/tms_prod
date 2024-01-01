e8cf75a5108f21d8a4622a7f6c223eba457236b4
Sohan Sarurkar <sohansarurkar47@gmail.com>
2024-01-01 17:47:12 +0530

    certs

--- app.js
+++ app.js
@@ -3,24 +3,10 @@ const cors = require('cors');
 const router = require('./routes');
 const fs = require('fs');
 const bodyParser = require('body-parser');
-// const SA = require('./superadmin/SA');
 const https = require('https')
-//const TMS_logs = require('./tms_trigger_logs');
-//const checkState = require('./SMS/smsController');
 
-/*               Interval                */
-
-// const MinuteData = require('./dash/interval_min');
-// const hourData = require('./dash/interval_hour');
-// const weekData = require('./dash/interval_week');
-// const dayData = require('./dash/interval_day');
-// const MonthData = require('./dash/interval_month');
-/*                 MQTT                  */
-// const mqtt_sub = require('./sub');
-// const mqtt_pub = require('./pub');
-
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/tms.senselive.in/privkey.pem', 'utf8');
-const fullchain = fs.readFileSync('/etc/letsencrypt/live/tms.senselive.in/fullchain.pem', 'utf8');
+const privateKey = fs.readFileSync('./cert/private-key.pem', 'utf8');
+const fullchain = fs.readFileSync('./cert/public-cert.pem', 'utf8');
 
 const credentials = { key: privateKey, cert: fullchain };
 
